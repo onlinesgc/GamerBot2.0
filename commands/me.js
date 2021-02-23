d32ff8c2ba2a5ed6acf06f9a04714af704a7856d
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-23 18:48:55 +0100

    Add me command to send user information of member.

---
+++ commands/me.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "me",
+	description: "Print information about user!",
+	async do(client, message, args, Discord, profileData) {
+		const embed = new Discord.MessageEmbed()
+			.setColor("#f54242")
+			.setTitle(`User info`)
+			.setDescription(`${message.member}'s user information.`)
+            .addFields(
+				{ name: "XP", value: profileData.xp }
+			)
+		message.channel.send(embed);
+	}
+}
