c2bd11e8fa31bb435289743d3bcb01be744eb877
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-23 17:50:55 +0100

    Fix a bug where emujoin command would emit invalid event.

--- commands/emujoin.js
+++ commands/emujoin.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: "emujoin",
 	description: "Emulate someone joining the guild!",
 	async do(client, message, args, Discord) {
-		client.emit("guildMemberAdd");
+		client.emit("guildMemberAdd", member);
 	}
 }
