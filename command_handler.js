7d80e546a4ed9dae1fbdfef93fc0c6371179cbec
Simon Uggla <simon.uggla@aol.com>
2021-02-24 19:21:40 +0100

    Added a more descriptive error.

--- command_handler.js
+++ command_handler.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 
-module.exports = (client, Discord) => {
-	const command_files = fs.readdirSync(`./commands/`).filter(file => file.endsWith(".js"));
-	for (const file of command_files) {
-		const command = require(`./commands/${file}`);
-		if (command.name) {
-			client.commands.set(command.name, command);
-		} else {
-			console.log("Command Name error!");
-		}
-	}
-}
+module.exports = (client) => {
+  const command_files = fs
+    .readdirSync(`./commands/`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of command_files) {
+    const command = require(`./commands/${file}`);
+    if (command.name) {
+      client.commands.set(command.name, command);
+    } else {
+      console.error(`The Command File "${file}" does not export name property`);
+    }
+  }
+};
