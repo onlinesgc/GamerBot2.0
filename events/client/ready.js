99690fb3f28089dc7fde9fb09b3ca87ebaa8f33c
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-24 01:28:30 +0100

    Set bot activity on ready event.

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,6 @@
-module.exports = () => {
-	console.log("Bot is online!");
+module.exports = (Discord, client) => {
+	console.log(`${client.user.username} is online! Hosting ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+	client.user.setActivity("Forre med böjsen!", { type: "PLAYING"}).catch(console.error);
 }
+
+//console.log(`Bot: Hosting ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
