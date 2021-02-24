753f230d80ba5659baa8fec43cb5cc9d6a8b6d2c
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-24 01:29:07 +0100

    Remove unneeded stuff.

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,5 +2,3 @@ module.exports = (Discord, client) => {
 	console.log(`${client.user.username} is online! Hosting ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
 	client.user.setActivity("Forre med böjsen!", { type: "PLAYING"}).catch(console.error);
 }
-
-//console.log(`Bot: Hosting ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
