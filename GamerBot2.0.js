7f86d2f2f4297ac586f43b0d19c6f9555daa5e58
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-24 18:02:12 +0100

    Klockan 12!

--- GamerBot2.0.js
+++ GamerBot2.0.js
@@ -12,50 +12,6 @@ client.commands = new Discord.Collection();
 	require(`./${handler}.js`)(client, Discord);
 });
 
-client.on("message", (message) => {
-	if (message.author.bot) {
-		return;
-	}
-	if (message.channel.id == "809393742637170708") {
-		message.react("✅");
-		message.react("❌");
-	}
-	else if (message.content.toLowerCase().replace(/\s/g, "").includes("gaming")) {
-		if (message.channel.id == "809483972282810390" || message.channel.id == "780765093343395880") {
-			if (Math.floor(Math.random() * 100) > 86) {
-				message.channel.send("**GAMING! 🎮**");
-			}
-		}
-	}
-	else if (message.content.toLowerCase().includes("christerpog") || message.content.toLowerCase().includes("cristerpog")) {
-		if (message.channel.id == "809483972282810390" || message.channel.id == "780765093343395880") {
-			message.react("810255466952917052")
-			message.channel.send("<:mello_ChristerPOG:810255466952917052>")
-		}
-	}
-	else if (message.mentions.has(client.user)) {
-		message.channel.send("Hej, jag är en bot som gamear på min fritid!")
-	}
-	else if (message.content.toLowerCase().includes("hur mycket är klockan")) {
-		var currentdate = new Date(); 
-		var datetime =  
-			("0" + currentdate.getHours()).slice(-2) + ":"  
-			+ ("0" + currentdate.getMinutes()).slice(-2) + ":" 
-			+ ("0" + currentdate.getSeconds()).slice(-2)
-		;
-		message.channel.send(`Klockan är ${datetime}`);
-	}
-	else if (message.content.toLowerCase().includes("vad är klockan")) {
-		var currentdate = new Date(); 
-		var datetime =  
-			("0" + currentdate.getHours()).slice(-2) + ":"  
-			+ ("0" + currentdate.getMinutes()).slice(-2) + ":" 
-			+ ("0" + currentdate.getSeconds()).slice(-2)
-		;
-		message.channel.send(`Klockan är ${datetime}`);
-	}
-});
-
 mongoose.connect(process.env.mongodb_srv, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
--- events/guild/message.js
+++ events/guild/message.js
@@ -1,6 +1,8 @@
 const profileModel = require("../../models/profileSchema");
 
 module.exports = async(Discord, client, message) => {
+	if (message.author.bot) return;
+	
 	var prefix = ".";
 
 	let profileData;
@@ -34,6 +36,39 @@ module.exports = async(Discord, client, message) => {
 			}
 		} else {
 			command.do(client, message, args, Discord, profileData);
+		}
+	}
+
+
+	if (message.channel.id == "809393742637170708") {
+		message.react("✅");
+		message.react("❌");
+	}
+
+	if (message.content.toLowerCase().replace(/\s/g, "").includes("gaming")) {
+		if (message.channel.id == "809483972282810390" || message.channel.id == "780765093343395880") {
+			if (Math.floor(Math.random() * 100) > 86) {
+				message.channel.send("**GAMING! 🎮**");
+			}
+		}
+	}
+	if (message.content.toLowerCase().includes("christerpog") || message.content.toLowerCase().includes("cristerpog")) {
+		if (message.channel.id == "809483972282810390" || message.channel.id == "780765093343395880") {
+			message.react("810255466952917052")
+			message.channel.send("<:mello_ChristerPOG:810255466952917052>")
+		}
+	}
+	if (message.content.toLowerCase().includes("hur mycket är klockan") || message.content.toLowerCase().includes("vad är klockan")) {
+		if (Math.floor(Math.random() * 100) > 91) {
+			message.channel.send("**KLOCKAN TOLV!**")
+		} else {
+			var currentdate = new Date();
+			var datetime =
+				("0" + currentdate.getHours()).slice(-2) + ":" 
+				+ ("0" + currentdate.getMinutes()).slice(-2) + ":" 
+				+ ("0" + currentdate.getSeconds()).slice(-2)
+			;
+			message.channel.send(`Klockan är ${datetime}`);
 		}
 	}
 }
