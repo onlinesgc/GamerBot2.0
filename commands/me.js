9269ce87e09ddb15d2eea22a70a07aa169f3ec02
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-26 16:35:26 +0100

    Do not include xp timeout field in me command if negative.

--- commands/me.js
+++ commands/me.js
@@ -6,13 +6,18 @@ module.exports = {
 	description: "Print information about user!",
 	perms: [],
 	async do(client, message, args, Discord, profileData) {
+		let fields = [
+			{ name: "XP", value: profileData.xp }
+		];
+		if (profileData.xpTimeoutUntil - message.createdTimestamp > 0) {
+			fields.push({ name: "XP Timeout", value: functions.msToString(profileData.xpTimeoutUntil - message.createdTimestamp) });
+		}
 		const embed = new Discord.MessageEmbed()
 			.setColor("#f54242")
 			.setTitle(`User info`)
 			.setDescription(`${message.member}'s user information.`)
 			.addFields(
-				{ name: "XP", value: profileData.xp },
-				{ name: "XP Timeout", value: functions.msToString(profileData.xpTimeoutUntil - message.createdTimestamp) }
+				fields
 			)
 		message.channel.send(embed);
 	}
