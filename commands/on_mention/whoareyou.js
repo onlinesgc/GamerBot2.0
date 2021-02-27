dafe672392928938ea5aa66376c9d98298fa0aee
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-27 03:14:38 +0100

    Add mention command: vem är du.

---
+++ commands/on_mention/whoareyou.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: "whoareyou",
+	permittedMessages: ["vem är du"],
+	description: "Prints information about this bot!",
+	perms: [],
+	async do(client, message, args, Discord) {
+		const embed = new Discord.MessageEmbed()
+			.setColor("#f54242")
+			.setTitle(`Om mig`)
+			.setDescription(`Jag är en bot skapad för Stamsites Discord av bot-teamet!`)
+            .setThumbnail(client.user.avatarURL())
+            .addFields(
+                { name: "Repo", value: "Om du vill hjälpa till att utveckla mig finns källkoden tillgänglig [här](https://github.com/stamdiscord/GamerBot2.0)!"}
+            )
+		message.channel.send(embed);
+	}
+}
