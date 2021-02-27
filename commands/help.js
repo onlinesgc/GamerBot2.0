583a177fe9a7822936b2d2e5cd73841c693d51db
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-27 17:48:24 +0100

    Remove ability from regular members to view admin command information.

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = {
 	perms: [],
 	async do(client, message, args, Discord, profileData) {
 		if (args[0]) {
-			return message.channel.send(getSpecificCmd(client, Discord, args[0]));
+			return message.channel.send(getSpecificCmd(client, Discord, args[0], message));
 		} else {
 			return message.channel.send(getAllCmds(client, Discord, message));
 		}
@@ -30,11 +30,13 @@ function getAllCmds(client, Discord, message) {
 	return embed;
 }
 
-function getSpecificCmd(client, Discord, input) {
+function getSpecificCmd(client, Discord, input, message) {
 	const cmd = client.commands.get(input.toLowerCase()) || client.commands.find(a => a.aliases && a.aliases.includes(input.toLowerCase()));
 
 	if (!cmd) {
 		return "Kommandot kunde inte hittas!";
+	} else if (cmd.perms.includes("adminCmd") && !message.member.hasPermission("ADMINISTRATOR")) {
+		return "Du har inte tillåtelse att visa information om hur det här kommandot används.";
 	} else {
 		let aliases = cmd.aliases.join(", ");
 		if (!aliases) {
