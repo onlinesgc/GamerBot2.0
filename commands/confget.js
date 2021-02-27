d5b270bef0c0a6644ee95402ca352cc758014fcc
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-27 18:02:48 +0100

    Make some changes in confget command.

--- commands/confget.js
+++ commands/confget.js
@@ -23,13 +23,28 @@ module.exports = {
 		configData2._id = undefined;
 		configData2.__v = undefined;
 
-		const embed = new Discord.MessageEmbed()
-			.setColor("#f54242")
-			.setTitle(`Config`)
-			.setDescription(`Konfigurationen för boten ser just nu ut såhär!`)
-			.addFields(
-				{ name: "Konfiguration:", value: `${JSON.stringify(configData2, null, 4)}` }
-			)
-		message.channel.send(embed);
+		if (args[0]) {
+			if (configData2[args[0]] == undefined) {
+				return message.channel.send(`Config-variabeln "${args[0]}" är odefinierad.`);
+			}
+			const embed = new Discord.MessageEmbed()
+				.setColor("#f54242")
+				.setTitle(`${args[0]} - Config`)
+				.setDescription(`Visar värdet av config-variabeln "${args[0]}".`)
+				.addFields(
+					{ name: "Värde:", value: `${JSON.stringify(configData2[args[0]], null, 4)}` }
+				)
+			return message.channel.send(embed);
+		} else {
+			const embed = new Discord.MessageEmbed()
+				.setColor("#f54242")
+				.setTitle(`Config`)
+				.setDescription(`Konfigurationen för boten ser just nu ut såhär!`)
+				.addFields(
+					{ name: "Konfiguration:", value: `${JSON.stringify(configData2, null, 4)}` }
+				)
+			return message.channel.send(embed);
+		}
+
 	}
 }
