681ba4217e0092a74757bcc3162dd4eb7f7ce766
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-27 03:02:44 +0100

    Show xp timeout in memberinfo command.

--- commands/memberinfo.js
+++ commands/memberinfo.js
@@ -1,4 +1,5 @@
 const profileModel = require("../models/profileSchema");
+const functions = require("../functions")
 
 module.exports = {
 	name: "memberinfo",
@@ -23,13 +24,20 @@ module.exports = {
 			profile.save();
 		}
 
+		let fields = [
+			{ name: "XP", value: profile_data.xp, inline: true }
+		];
+		if (profile_data.xpTimeoutUntil - message.createdTimestamp > 0) {
+			fields.push({ name: "XP Timeout", value: functions.msToString(profile_data.xpTimeoutUntil - message.createdTimestamp), inline: true });
+		}
+
 		const embed = new Discord.MessageEmbed()
 			.setColor("#f54242")
 			.setTitle(`Information om medlem`)
 			.setDescription(`${message.mentions.members.first()}'s information.`)
 			.setImage(message.mentions.users.first().avatarURL())
 			.addFields(
-				{ name: "XP", value: profile_data.xp },
+				fields,
 				{ name: "id", value: profile_data.userID }
 			)
 		message.channel.send(embed);
