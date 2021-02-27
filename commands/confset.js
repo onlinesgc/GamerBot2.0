9fb3f53bccd21d2636d7b55f21eafd9111e26171
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-27 04:09:25 +0100

    Add new command: confset.

---
+++ commands/confset.js
@@ -0,0 +1,26 @@
+const configModel = require("../models/configSchema");
+
+module.exports = {
+	name: "confset",
+	aliases: [],
+	description: "Change config options!",
+	usage: [],
+	perms: ["adminCmd"],
+	async do(client, message, args, Discord) {
+		if (!args[0]) return message.channel.send("Du måste ange vilken key du vill ändra.")
+		if (!args[1]) return message.channel.send("Du måste ange vilket värde key'n ska sättas till!")
+
+		//Retreive options
+		configData = await configModel.findOne({ id: 0 });
+		if (!configData) {
+			let config = await configModel.create({
+				prefix: ".",
+				id: 0
+			});
+			config.save();
+		}
+
+		configData[args[0]] = args[1];
+		message.channel.send(`Set "${args[0]}" value to "${args[1]}"`);
+	}
+}
