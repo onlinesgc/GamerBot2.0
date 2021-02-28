fecebd86451915d6ef5b8855e87339bfa6edd563
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-28 05:28:56 +0100

    Save video suggests in database.

--- commands/channel_action/videosuggest.js
+++ commands/channel_action/videosuggest.js
@@ -1,8 +1,11 @@
+const videoSuggestModel = require("../../models/videoSuggestSchema");
+
 module.exports = {
 	name: "videosuggest",
 	channels: ["809393742637170708", "815324315536326677"],
 	async do(client, message, Discord, profileData) {
 		message.react("✅");
 		message.react("❌");
+		videoSuggestModel.addVideoSuggest(message);
 	}
 }
---
+++ models/videoSuggestSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const videoSuggestSchema = new mongoose.Schema({
+	channelID: { type: String },
+    serverID: { type: String },
+    userID: { type: String },
+    messageID: { type: String, unique: true },
+	messageContent: { type: String }
+});
+
+const model = mongoose.model("videoSuggestModel", videoSuggestSchema);
+
+const addVideoSuggest = async (message) => {
+	videoSuggestData = await model.findOne({ messageID: message.id });
+	if (!videoSuggestData) {
+		let videoSuggestData = await model.create({
+			channelID: message.channel.id,
+            serverID: message.guild.id,
+			userID: message.author.id,
+			messageID: message.id,
+            messageContent: message.content
+		});
+		videoSuggestData.save();
+	}
+	return videoSuggestData;
+};
+
+module.exports = { videoSuggestModel: model, addVideoSuggest };
