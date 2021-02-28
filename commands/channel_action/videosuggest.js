6d408a820b8156c58c7693a88be270d54587e601
IsakTheHacker <isak.brynielsson@gmail.com>
2021-02-28 06:14:15 +0100

    Find similar video-suggestions when user makes a new suggestion.

--- commands/channel_action/videosuggest.js
+++ commands/channel_action/videosuggest.js
@@ -7,5 +7,37 @@ module.exports = {
 		message.react("✅");
 		message.react("❌");
 		videoSuggestModel.addVideoSuggest(message);
+
+		const similarMessages = new Set();
+		let fields = [
+			
+		];
+		
+		for await (const doc of videoSuggestModel.videoSuggestModel.find()) {
+			for (const word of message.content) {
+				if (doc.messageContent.includes(word)) {
+					similarMessages.add(doc);
+				}
+			}
+		}
+		
+		let n = 1;
+		for (const item of similarMessages) {
+			fields.push({ name: `Förekomst ${n}`, value: `Meddelande: *${item.messageContent}*`})
+			n++;
+		}
+		
+		if (fields.length) {
+			const embed = new Discord.MessageEmbed()
+				.setColor("#f54242")
+				.setTitle(`Liknande video-förslag`)
+				.setDescription(`Liknande video-förslag hittades!`)
+				.addFields(
+					fields
+				);
+			return message.channel.send(embed);
+		} else {
+			return message.channel.send("Bra! Inga liknande video-förslag hittades.")
+		}
 	}
 }
