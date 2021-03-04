d996079591891fe26386091afba88136f838135f
IsakTheHacker <isak.brynielsson@gmail.com>
2021-03-04 19:33:04 +0100

    Make a change in emujoin command.

--- commands/emujoin.js
+++ commands/emujoin.js
@@ -6,5 +6,6 @@ module.exports = {
 	perms: ["adminCmd"],
 	async do(client, message, args, Discord) {
 		client.emit("guildMemberAdd", message.member);
+		message.channel.send("Emulerade att någon anslöt till servern!");
 	}
 }
