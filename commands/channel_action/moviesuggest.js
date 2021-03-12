14e2df10ff86cd156a19d233b2e8f28002966fc2
Simon Uggla <simon.uggla@aol.com>
2021-03-12 13:59:27 +0100

    Removed client and Discord client can be accessed through message.client and Discord can be added through require'(discord')

--- commands/channel_action/moviesuggest.js
+++ commands/channel_action/moviesuggest.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "moviesuggest",
 	channels: ["816225717787689001", "816724246557884516"],
-	async do(client, message, Discord, profileData) {
+	async do(message, profileData) {
 		message.react("👍");
 		message.react("👎");
 	}
