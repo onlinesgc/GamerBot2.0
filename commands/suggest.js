10c50d81adea87fd58751ae6fc069a6c98360f66
Lukas <67807954+lukasabbe@users.noreply.github.com>
2021-03-21 22:16:38 +0100

    Borde stå att den inte finns en

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "suggest",
 	aliases: [],
-	description: "Suggest a feature!",
+	description: "Suggest a feature! (Funkar inte en)",
 	usage: [],
 	perms: [],
 	async do(message, args, profileData) {
