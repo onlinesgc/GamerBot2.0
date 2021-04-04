cb772cc10bab4dd65b3a746d78afd850bdf6a64e
IsakTheHacker <isak.brynielsson@gmail.com>
2021-04-04 17:03:48 +0200

    Use embeds in error messages.

--- logSystem.js
+++ logSystem.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 class Log {
 	static client = null;
 	static errorChannel = null;
@@ -6,9 +8,18 @@ class Log {
 		this.client = client;
 		this.errorChannel = errorChannel;
 	}
+
 	static error(body) {
 		let channel = this.client.channels.cache.get(this.errorChannel);
-		channel.send(body);
+
+		const embed = new Discord.MessageEmbed()
+				.setColor("#cf2525")
+				.setTitle(`Error`)
+				.setDescription(`Error occured!`)
+				.addFields(
+					{ name: "Error message:", value: body }
+				)
+		return channel.send(embed);
 	}
 }
 
