5740d29807d85ac31de58b9c4fe43b5884642d64
IsakTheHacker <isak.brynielsson@gmail.com>
2021-04-06 11:28:22 +0200

    Include error location (file and line number) in error messages.

--- logSystem.js
+++ logSystem.js
@@ -10,12 +10,21 @@ class Log {
 	}
 
 	static error(body) {
+		const line = (((new Error('log'))
+			.stack.split('\n')[2] || '…')
+			.match(/\(([^)]+)\)/) || [, 'not found'])[1];
+		// console.log(`${line}\n`, body);
+		
+		const lineNumber = line.split(":").slice(-2)[0];
+		const column = line.split(":").slice(-1)[0];
+		const file = line.replace(/\\/g, "/").split("/").slice(-1)[0].split(":")[0];
+
 		let channel = this.client.channels.cache.get(this.errorChannel);
 
 		const embed = new Discord.MessageEmbed()
 				.setColor("#cf2525")
 				.setTitle(`Error`)
-				.setDescription(`Error occured!`)
+				.setDescription(`Error occured in \`${file}\` at line \`${lineNumber}\`:\`${column}\`!`)
 				.addFields(
 					{ name: "Error message:", value: `\`\`\`${body}\`\`\`` }
 				)
