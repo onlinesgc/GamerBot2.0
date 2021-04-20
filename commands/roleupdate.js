4be55a5951b461707b9f4bf2da528c4f89635b4e
IsakTheHacker <isak.brynielsson@gmail.com>
2021-04-20 20:23:23 +0200

    Update roleupdate.js.

--- commands/roleupdate.js
+++ commands/roleupdate.js
@@ -13,26 +13,15 @@ module.exports = {
 	async do(message, args) {
 		//Get bot optionsi and user data
 		let configData = await configModel.fetchConfig(process.env.config_id);
-		let profileData = await profileModel.fetchAll();
 
-		//Get all members for guild where message was sent. (Requires Priviliged Gateway Intent being checked in bot settings on discord developer portal.	
-		message.guild.members.fetch()
-		.then(members => {members.forEach(member =>
-			{
-				//Remove level roles for current user.
-				let memberID = member.user.id;
-				
-				//Remove XP related roles from user.
-				configData.xp.levels.forEach(role => 
-					{
-						member.roles.remove(message.guild.roles.cache.get(role.id));
+		await message.guild.members.fetch().then(members => {
+			members.forEach(async user => {
+
+				configData.xp.levels.forEach(role => {		//Remove XP related roles
+					user.roles.remove(message.guild.roles.cache.get(role.id));
 				});
-
-				//Look up current user in database
-				let profileIndex = 0;
-				while (memberID != profileData[profileIndex].userID) {
-					profileIndex++;
-				} 	
+				
+				let profileData = await profileModel.fetchProfile(user.id, message.guild.id);
 
 				//Add new level roles adjused for new XP config.
 				for (let index = 0; index < configData.xp.levels.length; index++) {
@@ -45,15 +34,12 @@ module.exports = {
 						nextRoleLevel = configData.xp.levels[index+1].level;
 					}
 					//Actually testing level and adding roles.
-					if (profileData[profileIndex].level >= role.level+1 && profileData[profileIndex].level < nextRoleLevel+1) {
-						member.roles.add(message.guild.roles.cache.get(role.id));
+					if (profileData.level >= role.level+1 && profileData.level < nextRoleLevel+1) {
+						user.roles.add(message.guild.roles.cache.get(role.id));
 					}
 				}
-
-			}
-		)})
-		.catch(console.error);
+			})
+		}).catch(console.error);
 		message.channel.send("Successfully updated member roles!");
-
 	}
 }
