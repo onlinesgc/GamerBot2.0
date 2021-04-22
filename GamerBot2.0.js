41f0d6a2afc7b5b0e5a011137f27dfba730fcc25
IsakTheHacker <isak.brynielsson@gmail.com>
2021-04-23 00:51:08 +0200

    Move mongoDB startup code to ready event.

--- GamerBot2.0.js
+++ GamerBot2.0.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js");
-const mongoose = require("mongoose");
 const functions = require("./functions");
-const configModel = require("./models/configSchema");
 require('dotenv').config();
 
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
@@ -18,25 +16,5 @@ client.reaction_actions = new Discord.Collection();
 ["command_handler", "event_handler", "mention_handler", "question_handler", "channel_handler", "reaction_handler"].forEach(handler => {
 	require(`./handlers/${handler}.js`)(client);
 });
-
-mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.mongodb_srv, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false
-}).then(async() => {
-	console.log("Connected to the database!");
-
-	//Retreive options
-	let configData = await configModel.fetchConfig(process.env.config_id);		//Retreive options
-	console.log("Options retrieved!");
-
-	//Apply options
-	functions.applyOptions(client, configData);
-	console.log("Options applied!");
-}).catch((err) => {
-	console.log(process.env.mongodb_srv);
-	console.log(err);
-})
 
 client.login(token);
--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,27 @@
+const mongoose = require("mongoose");
+const configModel = require("../../models/configSchema");
+const functions = require("../../functions");
+
 module.exports = (client) => {
 	console.log(`${client.user.username} is online! Hosting ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+
+	mongoose.set('useCreateIndex', true);
+	mongoose.connect(process.env.mongodb_srv, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false
+	}).then(async () => {
+		console.log("Connected to the database!");
+
+		//Retreive options
+		let configData = await configModel.fetchConfig(process.env.config_id);		//Retreive options
+		console.log("Options retrieved!");
+
+		//Apply options
+		functions.applyOptions(client, configData);
+		console.log("Options applied!");
+	}).catch((err) => {
+		console.log(process.env.mongodb_srv);
+		console.log(err);
+	})
 }
