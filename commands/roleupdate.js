1f7e31dc0fb5506865a255f0c85e8ae69e15b949
Little_Fox <43337675+LittleFoxCode@users.noreply.github.com>
2021-04-23 15:12:46 +0200

    roleupdate now for singular user
    .roleupdate <userid>,<usertag>

--- commands/roleupdate.js
+++ commands/roleupdate.js
@@ -1,45 +1,56 @@
-const configModel = require("../models/configSchema");
-const Discord = require('discord.js');
 const profileModel = require("../models/profileSchema");
+const ms = require("ms");
+const Discord = require('discord.js');
+const configModel = require("../models/configSchema");
 
 module.exports = {
 	name: "roleupdate",
-	aliases: ["updateroles", "update-server-roles"],
-	description: "Uppdatera servermedlemmars rollinnehav, baserat på deras nuvarande level.",
+	aliases: [],
+	description: "Uppdatera roller för en enskild användare",
 	usage: [
-		".roleupdate",
+		".roleupdate <memberid>",
+		".roleupdate <membertag>",
 	],
+	notes: [],
 	perms: ["adminCmd"],
-	async do(message, args) {
-		//Get bot optionsi and user data
-		let configData = await configModel.fetchConfig(process.env.config_id);
+	async do(message, args, profileData) {
 
-		await message.guild.members.fetch().then(members => {
-			members.forEach(async user => {
+		//Retreive options
+		let configData = await configModel.fetchConfig(process.env.config_id);		//Retreive options
 
-				configData.xp.levels.forEach(role => {		//Remove XP related roles
-					user.roles.remove(message.guild.roles.cache.get(role.id));
-				});
-				
-				let profileData = await profileModel.fetchProfile(user.id, message.guild.id);
-
-				//Add new level roles adjused for new XP config.
-				for (let index = 0; index < configData.xp.levels.length; index++) {
-					const role = configData.xp.levels[index];
-					//nextRoleLevel allows for testing within span, and if statement helps in end of list.
-					let nextRoleLevel = 0;
-					if (index === configData.xp.levels.length-1) {
-						nextRoleLevel = 10000000;
-					} else {
-						nextRoleLevel = configData.xp.levels[index+1].level;
-					}
-					//Actually testing level and adding roles.
-					if (profileData.level >= role.level+1 && profileData.level < nextRoleLevel+1) {
-						user.roles.add(message.guild.roles.cache.get(role.id));
-					}
-				}
-			})
-		}).catch(console.error);
-		message.channel.send("Successfully updated member roles!");
+		let member;
+		let user;
+		if (!args[0]) {
+			return message.channel.send("Du måste ange vilken användare du vill sätta xp'n för.");
+		} else {
+			if (message.mentions.members.first()) {
+				member = message.mentions.members.first();
+				user = message.mentions.users.first();
+			} else {
+				member = await message.guild.members.fetch(args[0]);
+				user = await message.client.users.fetch(args[0]);
+			}
+		}
+		let profile_data = await profileModel.fetchProfile(member.id, message.guild.id);		//Fetch profile
+		//Update level
+		configData.xp.levels.forEach(element => {		//Remove all level roles
+			member.roles.remove(message.guild.roles.cache.get(element.id));
+		});
+		//Adding find correct role
+		for (let index = 0; index < configData.xp.levels.length; index++) {
+			const role = configData.xp.levels[index];
+			//nextRoleLevel allows for testing within span, and if statement helps in end of list.
+			let nextRoleLevel = 0;
+			if (index === configData.xp.levels.length-1) {
+				nextRoleLevel = 100000000;
+			} else {
+				nextRoleLevel = configData.xp.levels[index+1].level;
+			}
+			//Actually adding roles
+			if (profile_data.level >= role.level+1 && profile_data.level < nextRoleLevel+1) {
+				member.roles.add(message.guild.roles.cache.get(role.id));
+				message.channel.send("Added role: " + message.guild.roles.cache.get(role.id).name + ". To member: " + member.user.username);
+			}
+		}
 	}
 }
