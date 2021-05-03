7084e8af45831ac7caf1f38bcd9e15376e55dc2e
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2021-05-03 15:59:37 +0200

    fix with time

--- functions.js
+++ functions.js
@@ -117,7 +117,7 @@ module.exports = {
 				configData.save();
 				client.guilds.cache.get("516605157795037185").channels.cache.get("814163313675730954").send(`STAMSITE har lagt upp en ny video! <:Marcus_Pog:813821837976535060>\n**[${title}]** <@&813098115934191626>\n http://www.youtube.com/watch?v=${id}`);
 			}
-		}, 1000 * 10);	
+		}, 1000 * 60 * 15);	
 		async function executeGoogle(){
 			var resId;
 			yt = await google.youtube({
