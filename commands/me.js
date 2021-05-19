2e360472b4db55bbb005d9f6e473140d476432eb
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2021-05-19 21:53:28 +0200

    start på en ny me

--- commands/me.js
+++ commands/me.js
@@ -1,6 +1,8 @@
 const functions = require("../functions");
 const Discord = require('discord.js');
 const configModel = require("../models/configSchema");
+const {createCanvas, loadImage} = require("canvas");
+const fs = require("fs");
 
 module.exports = {
 	name: "me",
@@ -28,7 +30,26 @@ module.exports = {
 
 		let xpPercentage = Math.round(profileData.xp / Math.pow(profileData.level + configData.xp.levelBaseOffset, configData.xp.levelExponent) * 100);
 		let progressBar = "█".repeat(Math.round(xpPercentage / 10)) + "░".repeat(Math.round((100 - xpPercentage) / 10));
-		
+
+		var whidth = 500;
+        var hight = 800;
+        const Profile = createCanvas(whidth,hight);
+        const ProfileOptions = Profile.getContext("2d");
+        ProfileOptions.fillStyle = '#4A4E48';
+        ProfileOptions.fillRect(0,0,whidth,hight);
+        await loadImage(message.author.avatarURL({format:"png"})).then(img =>{
+            ProfileOptions.drawImage(img,(whidth/2) - 100,100,200,200);
+        })
+        ProfileOptions.font = "bold 50pt Sans"
+        ProfileOptions.textAlign = "center"
+        ProfileOptions.fillStyle = "#fff"
+        ProfileOptions.fillText(message.author.username,(whidth/2), 400)
+		ProfileOptions.font = "normal 40pt Sans"
+		ProfileOptions.fillText(`Level:\n${profileData.level-1}`,(whidth/2)-100,500);
+		ProfileOptions.fillText(`Progress:\n${progressBar}${xpPercentage}`,(whidth/2),630);
+        const buffer = Profile.toBuffer("image/png");
+        await fs.writeFileSync("../Temp.png",buffer);
+		/*
 		const embed = new Discord.MessageEmbed()
 			.setColor("#f54242")
 			.setTitle(`Användarinfo`)
@@ -40,6 +61,7 @@ module.exports = {
 				{ name: "Progress", value: `${progressBar} ${xpPercentage}%` },
 				{ name: "id", value: message.author.id }
 			)
-		message.channel.send(embed);
+		*/
+		message.channel.send({files:["../Temp.png"]});
 	}
 }
