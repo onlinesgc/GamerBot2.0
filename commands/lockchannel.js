b303583170eb0df3e48345504d707d7a94386b66
IsakTheHacker <isak.brynielsson@gmail.com>
2021-05-19 22:43:20 +0200

    Add hidechannel and showchannel command.

--- commands/lockchannel.js
+++ commands/lockchannel.js
@@ -1,14 +1,12 @@
-const Discord = require('discord.js');
-
 module.exports = {
-	name: "lock",
-	aliases: [],
+	name: "lockchannel",
+	aliases: ["lock"],
 	description: "Lås en kanal så att den blir skrivskyddad.",
 	usage: [],
 	perms: ["adminCmd"],
 	async do(message, args, profileData) {
 		message.channel.updateOverwrite(message.guild.id, {
-			SEND_MESSAGES: false,
+			SEND_MESSAGES: false
 		});
 
 		message.channel.send("Denna kanal är nu låst/skrivskyddad.");
--- commands/unlockchannel.js
+++ commands/unlockchannel.js
@@ -1,14 +1,12 @@
-const Discord = require('discord.js');
-
 module.exports = {
-	name: "unlock",
-	aliases: [],
+	name: "unlockchannel",
+	aliases: ["unlock"],
 	description: "Lås upp en kanal så att användare kan skriva i den.",
 	usage: [],
 	perms: ["adminCmd"],
 	async do(message, args, profileData) {
 		message.channel.updateOverwrite(message.guild.id, {
-			SEND_MESSAGES: true,
+			SEND_MESSAGES: true
 		});
 
 		message.channel.send("Denna kanal har nu blivit upplåst/meddelanden accepteras.");
