37b91a39fe697ad78e031bba0546934c987a5fd5
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2021-05-20 18:03:07 +0200

    small things to the new .me command

--- commands/me.js
+++ commands/me.js
@@ -1,7 +1,7 @@
 const functions = require("../functions");
 const Discord = require('discord.js');
 const configModel = require("../models/configSchema");
-const {createCanvas, loadImage} = require("canvas");
+const {createCanvas, loadImage, registerFont} = require("canvas");
 const fs = require("fs");
 
 module.exports = {
@@ -33,21 +33,36 @@ module.exports = {
 
 		var whidth = 500;
         var hight = 800;
+		registerFont("./canvas/Hard Compound.ttf",{family:"Hard_Compound"});
         const Profile = createCanvas(whidth,hight);
         const ProfileOptions = Profile.getContext("2d");
         ProfileOptions.fillStyle = '#4A4E48';
         ProfileOptions.fillRect(0,0,whidth,hight);
         await loadImage(message.author.avatarURL({format:"png"})).then(img =>{
-            ProfileOptions.drawImage(img,(whidth/2) - 100,100,200,200);
+			ProfileOptions.fillStyle = "#5FDA18";
+			ProfileOptions.fillRect((whidth/2)-135,70,270,270)
+            ProfileOptions.drawImage(img,(whidth/2) - 125,80,250,250);
         })
-        ProfileOptions.font = "bold 50pt Sans"
+        ProfileOptions.font = "bold 50pt Hard_Compound"
         ProfileOptions.textAlign = "center"
         ProfileOptions.fillStyle = "#fff"
         ProfileOptions.fillText(message.author.username,(whidth/2), 400)
-		ProfileOptions.font = "normal 40pt Sans"
-		ProfileOptions.fillText(`Level:\n${profileData.level-1}`,(whidth/2)-100,500);
-		ProfileOptions.font = "normal 30pt Sans"
-		ProfileOptions.fillText(`Progress:\n${progressBar} ${xpPercentage}%`,(whidth/2),630);
+		ProfileOptions.font = "normal 40pt Hard_Compound"
+		ProfileOptions.fillText(`Level: ${profileData.level-1}`,(whidth/2),500);
+		//ProfileOptions.font = "normal 30pt sans"
+		//ProfileOptions.fillText(`Progress:\n${progressBar} ${xpPercentage}%`,(whidth/2),630);
+		var multiplier = 3.5;
+		var fildBar = 100 * multiplier;
+		var Bar = xpPercentage * multiplier;
+		ProfileOptions.fillStyle = "#898C87"
+		ProfileOptions.fillRect(70,550,fildBar,40);
+		ProfileOptions.fillStyle = "#fff"
+		ProfileOptions.fillRect(70,550,Bar,40);
+		ProfileOptions.font = "normal 40pt Hard_Compound";
+		ProfileOptions.fillText(`${xpPercentage}%`,(whidth/2),645);
+		await loadImage("./canvas/BackrundsFrame.png").then(img =>{
+            ProfileOptions.drawImage(img,0,0,whidth,hight);
+        })
         const buffer = Profile.toBuffer("image/png");
         await fs.writeFileSync("../Temp.png",buffer);
 		/*
