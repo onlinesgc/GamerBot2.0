d89eb1d3ebe7540ee80e77a3b27f3533bf0c75c2
LittleFoxCode <holtzjohan@gmail.com>
2021-06-22 14:25:02 +0200

    Instead of printing out all settings, we now show a list
    Cleaned up indentation, changed some variable names for clarity
    creating and displaying the list when no specific option was chosen

--- commands/confget.js
+++ commands/confget.js
@@ -13,17 +13,17 @@ module.exports = {
     async do(message, args, profileData) {
 	
 	//Retreive options
-	let configData = await configModel.fetchConfig(process.env.config_id);		//Retreive options
+	let configDataRaw = await configModel.fetchConfig(process.env.config_id);		//Retreive options
 	
-	let configData2 = JSON.parse(JSON.stringify(configData));
-	configData2.id = undefined;
-	configData2._id = undefined;
-	configData2.__v = undefined;
+	let configDataParsed = JSON.parse(JSON.stringify(configDataRaw));
+	delete configDataParsed.id;
+	delete configDataParsed._id;
+	delete configDataParsed.__v;
 	
 	
 	
 	if (args[0]) {
-	    if (configData2[args[0]] == undefined) {
+	    if (configDataParsed[args[0]] == undefined) {
 		return message.channel.send(`Config-variabeln "${args[0]}" är odefinierad.`);
 	    }
 	    const embed = new Discord.MessageEmbed()
@@ -31,20 +31,24 @@ module.exports = {
 		  .setTitle(`${args[0]} - Config`)
 		  .setDescription(`Visar värdet av config-variabeln *${args[0]}*.`)
 		  .addFields(
-		      { name: "Värde:", value: `\`\`\`json\n${JSON.stringify(configData2[args[0]], null, 4)}\`\`\`` }
+		      { name: "Värde:", value: `\`\`\`json\n${JSON.stringify(configDataParsed[args[0]], null, 4)}\`\`\`` }
 		  )
 	    return message.channel.send(embed);
 	} else {
+	    var settingsList = "";
+	    Object.keys(configDataParsed).forEach(setting => {
+		settingsList = settingsList + setting + "\n";
+	    })
+	    
 	    const embed = new Discord.MessageEmbed()
 		  .setColor("#f54242")
 		  .setTitle(`Config`)
-		  .setDescription(`Konfigurationen för boten ser just nu ut såhär!`)
+		  .setDescription(`Du måste välja en config kategori under efter huvudkommandot. Dessa kategorier finns:`)
 		  .addFields(
-		      { name: "Konfiguration:", value: `\`\`\`json\n${JSON.stringify(configData2, null, 4)}\`\`\`` },
-		      { name: "ID:", value: `Den här boten använder konfigurationsdokumentet med id \`${configData.id}\`.` }
+		      { name: "Konfiguration:", value: settingsList},
+		      { name: "ID:", value: `Den här boten använder konfigurationsdokumentet med id \`${configDataRaw.id}\`.` }
 		  )
 	    return message.channel.send(embed);
 	}
-	
     }
 }
