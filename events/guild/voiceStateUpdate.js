85191b9b7e4a1df6c5f5597ab467c183d5f922bf
IsakTheHacker <isak.brynielsson@gmail.com>
2021-07-07 16:13:57 +0200

    Add voiceSwitch event.

--- events/guild/voiceStateUpdate.js
+++ events/guild/voiceStateUpdate.js
@@ -2,15 +2,13 @@ module.exports = async (oldMember, newMember) => {
 	let newUserChannel = client.guilds.cache.get(newMember.guild.id).channels.cache.get(newMember.channelID);
 	let oldUserChannel = client.guilds.cache.get(oldMember.guild.id).channels.cache.get(oldMember.channelID);
 
-	if (oldUserChannel === undefined && newUserChannel) {
-
-		// User Joins a voice channel
+	if (oldUserChannel === undefined && newUserChannel) {				//Someone joined a voicechannel
 		require("../custom/voiceJoin")(oldMember, newMember);
 
-	} else if (newUserChannel === undefined) {
-
-		// User leaves a voice channel
+	} else if (newUserChannel === undefined && oldUserChannel) {		//Someone left a voice channel
 		require("../custom/voiceLeave")(oldMember, newMember);
 
+	} else if (oldUserChannel && newUserChannel) {						//Someone switched to another voice channel
+		require("../custom/voiceSwitch")(oldMember, newMember);
 	}
 }
