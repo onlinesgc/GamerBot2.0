2a1ee45a54f9f44ebae994d08aeeffe218905bb2
IsakTheHacker <isak.brynielsson@gmail.com>
2021-07-09 23:42:53 +0200

    Make some small changes in the serverinfo command.

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -10,12 +10,14 @@ module.exports = {
 		const embed = new Discord.MessageEmbed()
 			.setColor("#0099ff")
 			.setTitle(`Serverinfo`)
-			.setImage(message.guild.iconURL())
+			.setThumbnail(message.guild.iconURL())
 			.addFields(
 				{ name: "Medlemmar", value: `\`${message.guild.memberCount}\`` },
 				{ name: "Status", value: `
 					🟢 \`${message.guild.members.cache.filter(m => m.presence.status === "online").size}\` medlemmar är online!
 					🕓 \`${message.guild.members.cache.filter(m => m.presence.status === "online" && m.hasPermission("ADMINISTRATOR") && !m.user.bot).size}\` admins är tillgängliga!
+
+					🔴 \`${message.guild.members.cache.filter(m => m.presence.status === "offline").size}\` personer är offline.
 				`}
 			)
 		message.channel.send(embed);
