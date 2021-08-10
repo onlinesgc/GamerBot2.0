b33aab683fd98293e34207246446642351fafafa
IsakTheHacker <isak.brynielsson@gmail.com>
2021-08-10 20:10:43 +0200

    Change support role ids.

--- events/guild/guildMemberUpdate.js
+++ events/guild/guildMemberUpdate.js
@@ -1,7 +1,7 @@
 const configModel = require("../../models/configSchema");
 module.exports = async ( oldMember, newMember, client) => {
-    let supportRoles = ["864797910914695168","864799262625759232"]
-    let megaSuportRole = "864800059840004107"
+    let supportRoles = ["817886647915642930","817887552370901042"];
+    let megaSuportRole = "864800059840004107";
     let b = 0;
     let t = 0;
     let configdata = await configModel.fetchConfig(process.env.config_id);
@@ -21,16 +21,16 @@ module.exports = async ( oldMember, newMember, client) => {
         })
     })
     if(b == supportRoles.length){
-        oldMember.roles.cache.forEach(async e =>{
-            await supportRoles.forEach(e2 =>{
-                if(e2 == e.id){
-                    t++;
-                }
-            })
-        })
-        if(t != supportRoles.length){
-            newMember.guild.channels.cache.get("816724723739656222").send(`Tack <@${newMember.id}>, för att du supportar stamsite till det ytersta :D\nMed deta kommer du få en egen roll på servern`);
-            newMember.roles.add(megaSuportRole);
-        }
+        // oldMember.roles.cache.forEach(async e =>{
+        //     await supportRoles.forEach(e2 =>{
+        //         if(e2 == e.id){
+        //             t++;
+        //         }
+        //     })
+        // })
+        // if(t != supportRoles.length){
+        //     newMember.guild.channels.cache.get("816724723739656222").send(`Tack <@${newMember.id}>, för att du supportar stamsite till det ytersta :D\nMed deta kommer du få en egen roll på servern`);
+        //     newMember.roles.add(megaSuportRole);
+        // }
     }
 }
