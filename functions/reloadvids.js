bf7c0928b183e2d2474ed2ea6519ebba996dabbe
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2021-12-29 10:19:13 +0100

    made the time longer

--- functions/reloadvids.js
+++ functions/reloadvids.js
@@ -57,7 +57,7 @@ module.exports = async (client) => {
                 }
             }
         },1000 )
-    }, 1000 * 10)
+    }, 1000 * 60*3)
 
     function getYoutubeVideoData(channel) {
         const request = require("request");
